45e6e05712eaee7e46ec38b9b1566ccff1998383
Aditya Patawari <aditya@adityapatawari.com>
2015-12-10 15:30:27 +0530

    need comic numbers

--- download_all_xkcd.sh
+++ download_all_xkcd.sh
@@ -2,5 +2,5 @@
 
 for i in {12..1544}
 do 
-curl http://xkcd.com/$i/ | grep "Image URL (for hotlinking/embedding):" |awk '{print $(NF)}'|xargs wget
+curl http://xkcd.com/$i/ | grep "Image URL (for hotlinking/embedding):" |awk '{print $(NF)}'|xargs wget -O $i.png
 done
